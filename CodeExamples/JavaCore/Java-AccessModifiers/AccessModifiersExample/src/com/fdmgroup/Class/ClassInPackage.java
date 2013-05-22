a24584008ecf9c63e1ab28391346eb554fc8286e
javatrainer <javatrainer@7f25c410-5c24-4347-b776-a721ee50d0ca>
2013-05-22 12:31:00 +0000

    Initial check-in

---
+++ CodeExamples/JavaCore/Java-AccessModifiers/AccessModifiersExample/src/com/fdmgroup/Class/ClassInPackage.java
@@ -0,0 +1,15 @@
+package com.fdmgroup.Class;
+
+public class ClassInPackage {
+
+	public static void main(String[] args)
+	{
+		SourceClass sc = new SourceClass();
+		
+		sc.methodForAll();				//public call from SourceClass
+		sc.methodForFriendsAndFamily();	//protected call from SourceClass
+		sc.methodForOnlyFriends();		//default call from SourceClass
+		//sc.methodForOnlyClass();		//private call from SourceClass which will never work
+		sc.methodCallLocal();			//method from in SourceClass can call all methods
+	}
+}
