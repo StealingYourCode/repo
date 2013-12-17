d4553ad8122210958ef153c24d906cbc160db142
javatrainer <javatrainer@7f25c410-5c24-4347-b776-a721ee50d0ca>
2013-12-17 19:25:43 +0000

    First commit.

--- CodeExamples/NYTemporaryExamples/LoggingExample/src/com/fdmgroup/logging/LoggingExample.java
+++ CodeExamples/NYTemporaryExamples/LoggingExample/src/com/fdmgroup/logging/LoggingExample.java
@@ -4,61 +4,40 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class LoggingExample {
-
 	
-	// Instantiates and retrieves a Logger called 
-	// "LoggingExample.class" 
+	
 	static Logger log = 
 			Logger.getLogger(LoggingExample.class);
+	
 	
 	LoggingExample(){
 		
 		PropertyConfigurator.configure("log4j.properties");
+
 	}
 
-	
-	public static void main(String[] args) {
-	
-		log.trace("Entered main method...");
-		
-		log.debug("Debug message!");
-		
-		log.debug("Check if everything is okay");
-		
-		log.info("Continuing with main execution");		
-		
-		log.warn("Warning! Something might be wrongs");
-	
-		log.error("Hello error");
-		
-		log.fatal("Everything died");
+	void thisMethodWillLog(){
 
+		log.trace("We just entered a method.");
+
+		log.info("Some info.");
 		
+		log.warn("Warning. Errors ahead.");
 		
-//		Object[] objArray = new Object[2];
-//
-//		if (objArray[1] != null)
-//			objArray[1].toString();	
-//		else
-//			log.error("Null reference here..");
-//		
-//		int i = 17;
-//		
-//		if (i < objArray.length)
-//			objArray[17].toString();
-//		else
-//			log.error("Array index out of bounds");
-//		
-//		
-//		try {
-//			
-//			objArray[17].toString();
-//		}
-//		catch(Exception e){
-//			log.error(e);
-//			log.error("message",e);
-//		}
+		log.error("Terrible error.");
+		
+		log.fatal("This error is even worse.");
 		
 	}
-
+	
+	
+	public static void main(String[] args){
+		
+		LoggingExample anInstance = 
+				new LoggingExample();
+		
+		anInstance.thisMethodWillLog();
+		
+	}
+	
 }
