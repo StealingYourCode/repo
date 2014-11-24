c705c54f96bb3bdbf26adaacd8104ff5858800a2
javatrainer <javatrainer@7f25c410-5c24-4347-b776-a721ee50d0ca>
2014-11-24 20:44:57 +0000

    reset

--- CodeExamples/NYTemporaryExamples/ExceptionsExample/src/com/fdmgroup/exceptions/ExceptionPropagationExample.java
+++ CodeExamples/NYTemporaryExamples/ExceptionsExample/src/com/fdmgroup/exceptions/ExceptionPropagationExample.java
@@ -1,65 +1,60 @@
 package com.fdmgroup.exceptions;
 
-/** This Demo uses RuntimeExceptions to demonstrate try-catch blocks.
- * 	Normally we handle RuntimeExceptions with defensive coding, but here
- * 	they help us simulate checked exception handling. */
+/**
+ * This Demo uses RuntimeExceptions to demonstrate try-catch blocks. Normally we
+ * handle RuntimeExceptions with defensive coding, but here they help us
+ * simulate checked exception handling.
+ */
 
 public class ExceptionPropagationExample {
 
-    public static void main (String[] args ) {
-    	divideSafely( new String[] {"1","1"} );  
-    }
+	public static void main(String[] args) {
 
-    private static void divideSafely(String[] array) {
-    	
-        try {
-        	
-        	System.out.println(divideArray(array));
-            System.out.println("End of try...");
-        
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Incorrect usage. Need two integer args.");
-        } catch (NumberFormatException e) {
-            System.out.println("Args must be integers.");
-        } catch (ArithmeticException e) {
-            System.out.println("Cannot divide by zero.");
-        } finally {
-        	// always runs
-        	System.out.println("Finally!");
-        }
-        
-        System.out.println("After try-catch...");
-        
-    }
+		divideSafely(new String[] { "1","1" });
+	}
 
-    private static int divideArray(String[] array) throws ArithmeticException, NumberFormatException, ArrayIndexOutOfBoundsException {
-       
-    	String s1 = array[0];
-        String s2 = array[1];
-        return divideStrings(s1, s2);
-    }
+	private static void divideSafely(String[] array) {
 
-    private static int divideStrings(String s1, String s2) throws ArithmeticException, NumberFormatException {
-       
-    	int i1 = Integer.parseInt(s1);
-        int i2 = Integer.parseInt(s2);
-        return divideInts(i1, i2);
-    }
+		try {
 
-    private static int divideInts(int i1, int i2) throws ArithmeticException
-    {
-        return i1 / i2;
-    }
-	
+			System.out.println("Result: " + divideArray(array));
+
+			
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Incorrect usage. Need two integer args.");
+		} catch (NumberFormatException e) {
+			System.out.println("Args must be integers.");
+		} catch (ArithmeticException e) {
+			System.out.println("Cannot divide by zero.");
+		} finally {
+			// always runs
+			System.out.println("Finally!");
+		}
+
+		System.out.println("Life after try-catch...");
+
+	}
+
+	private static int divideArray(String[] array) throws ArithmeticException,
+			NumberFormatException, ArrayIndexOutOfBoundsException {
+
+		String s1 = array[0];
+		String s2 = array[1];
+		return divideStrings(s1, s2);
+	}
+
+	private static int divideStrings(String s1, String s2)
+			throws ArithmeticException, NumberFormatException {
+
+		int i1 = Integer.parseInt(s1);
+		int i2 = Integer.parseInt(s2);
+		return divideInts(i1, i2);
+	}
+
+	private static int divideInts(int i1, int i2) throws ArithmeticException {
+
+		return i1 / i2;
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
--- CodeExamples/NYTemporaryExamples/ExceptionsExample/src/com/fdmgroup/exceptions/throwing/BadArgException.java
+++ CodeExamples/NYTemporaryExamples/ExceptionsExample/src/com/fdmgroup/exceptions/throwing/BadArgException.java
@@ -3,6 +3,11 @@ package com.fdmgroup.exceptions.throwing;
 
 public class BadArgException extends Exception {
 	
+	
+	public double getTheArg(){
+		return 0.0;
+	}
+	
 	BadArgException(){
 		super("Argument is bad.");
 	}
